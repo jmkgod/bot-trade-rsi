eb39fbc5e18152ac43ea658f775925891015f6db
janny <janny4751@gmail.com>
2021-05-25 20:26:55 +0700

    fix sell

--- BinanceTrade/Trade.py
+++ BinanceTrade/Trade.py
@@ -8,10 +8,7 @@ except Exception:
 client = Client( API_BINANCE_KEY , API_BINANCE_SECRET )
 
 def BUY(symbol,position_size):
-    print('symbol',symbol)
-    print('position_size',position_size)
     USDT_balance = client.get_asset_balance("USDT")
-    print('USDT_balance', USDT_balance)
     if float(USDT_balance['free']) >= 10 :
         order = client.order_market_buy(
             symbol=symbol,
@@ -22,41 +19,29 @@ def BUY(symbol,position_size):
 
     return "เกิดข้อผิดพลาด"
 
-def SELL(symbol,position_size=0,sell_all=True):
-    print('symbol',symbol)
-    print('position_size',position_size)
+def SELL(symbol,position_size=0):
     POS_SIZE = str(position_size)
     sym = symbol.split("USDT")[0] #BTCUSDT --> BTC
-    if sell_all:
-        POS_SIZE = client.get_asset_balance(sym)['free']
-    # sym = symbol.split("USDT")[0] #BTCUSDT --> BTC
-    # 0.00223445 --> [0 , 00223445]
-    Interger = POS_SIZE.split(".")[0]
-    decimal = POS_SIZE.split(".")[1]
-    dec_count = -1
+    # if sell_all:
+    #     POS_SIZE = client.get_asset_balance(sym)['free']
+    # # sym = symbol.split("USDT")[0] #BTCUSDT --> BTC
+    # # 0.00223445 --> [0 , 00223445]
+    # Interger = POS_SIZE.split(".")[0]
+    # decimal = POS_SIZE.split(".")[1]
+    # dec_count = -1
 
-    while True:
-        position_size = Interger + "." + decimal[:dec_count]
-        print('position_size',position_size)
-        if float(position_size) > 0:
-            try:
-                order = client.order_market_sell(
-                    symbol=symbol,
-                    quantity=position_size
-                )
 
-                return order
+        # position_size = Interger + "." + decimal[:dec_count]
+    if float(position_size) > 0 :
+        order = client.order_market_sell(
+                symbol=symbol,
+                quantity=position_size
+            )
+        
+        return order
             
-            except Exception as e:
-                if e.code == -1013:
-                    dec_count = dec_count - 1
-
-                else :
-                    print(e.args)
-                    return "เกิดข้อผิดพลาด"
 
 def ReceiveSignals(signal_data_dict):
-    print('signal_data_dict',signal_data_dict)
     if signal_data_dict["SIGNALS"] == "buy":
         try:
             BUY(symbol=signal_data_dict["SYMBOL"],position_size=signal_data_dict["POSITION_SIZE"])
